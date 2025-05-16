f7465a22de3544c6b7227f5439fa13535ab24e79
donini <donini@gmail.com>
2025-05-16 16:51:30 -0300

    Attempt fot the port issue

--- app.js
+++ app.js
@@ -3,13 +3,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var indexRouter = require('./routes/index');
-
 const matter = require("gray-matter");
 const md = require("markdown-it")({ html: true });
-
+const port = process.env.PORT;
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 4000;
 global.__basedir = __dirname;
 
 
